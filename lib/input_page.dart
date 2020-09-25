7a62f6290162d21880197de802cddfb90e6a3d76
Thomas M <twsm000@gmail.com>
2020-09-25 01:45:19 -0300

    Create a widget to maintain the code reusable

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -6,14 +6,74 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Color _cardColor = Color(0xFF1D1E33);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: ReusableCard(
+                    color: this._cardColor,
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: ReusableCard(
+                    color: this._cardColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: ReusableCard(
+              color: this._cardColor,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              children: [
+                Expanded(
+                    flex: 1,
+                    child: ReusableCard(
+                      color: this._cardColor,
+                    )),
+                Expanded(
+                    flex: 1,
+                    child: ReusableCard(
+                      color: this._cardColor,
+                    )),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color color;
+  ReusableCard({@required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: this.color,
       ),
     );
   }

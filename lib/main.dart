7f95166528ecc5d760989bfea51304a01ffc74df
Thomas M <twsm000@gmail.com>
2020-09-25 00:37:35 -0300

    Modifying default dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,10 @@ class BMICalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: InputPage(),
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
     );
   }
 }
@@ -25,9 +29,6 @@ class _InputPageState extends State<InputPage> {
       ),
       body: Center(
         child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
       ),
     );
   }

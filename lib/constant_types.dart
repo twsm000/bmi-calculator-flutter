4116d8523484df0de25f134c80a43a18ec44371d
Thomas M <twsm000@gmail.com>
2020-10-06 22:26:45 -0300

    Rename of constants

--- lib/constant_types.dart
+++ lib/constant_types.dart
@@ -4,17 +4,17 @@ const activeCardColor = Color(0xFF1D1E33);
 const inactiveCardColor = Color(0xFF111328);
 const bottomContainerColor = Color(0xDDEB1555);
 const activeSliderColor = Color(0xCCEB1555);
-const inactiveSliderColor = Color(0xFF8D8E98);
+const inactiveLabelColor = Color(0xFF8D8E98);
 const overlaySliderColor = Color(0x33EB1555);
 const buttonColor = Color(0xFF4C4F5E);
 
-const bottomContainerHeight = 60.0;
+const bottomContainerHeight = 55.0;
 const minHeight = 120.0;
 const maxHeight = 220.0;
 
 const labelTextStyle = TextStyle(
   fontSize: 18,
-  color: Color(0xFF8D8E98),
+  color: inactiveLabelColor,
 );
 
 const numberTextStyle = TextStyle(

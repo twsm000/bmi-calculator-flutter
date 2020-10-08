bcd60fcb3e2a1b9effdb489d335e4e32aee08a58
Thomas M <twsm000@gmail.com>
2020-10-08 01:39:25 -0300

    Create layout of ResultPage

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -1,13 +1,140 @@
 import 'package:flutter/material.dart';
 
-class ResultPage extends StatelessWidget {
+import 'constant_types.dart';
+import 'full_width_button.dart';
+import 'reusable_card.dart';
+import 'routes.dart';
+
+class ResultPage extends StatefulWidget {
+  @override
+  _ResultPageState createState() => _ResultPageState();
+}
+
+class _ResultPageState extends State<ResultPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("BMI CALCULATOR"),
       ),
-      body: Text("Hello"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _getTitle(),
+          _getResultCard(),
+          _getButtonRecalculate(),
+        ],
+      ),
+    );
+  }
+
+  Widget _getTitle() {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        child: Center(
+          child: Text(
+            "Your Result",
+            style: TextStyle(
+              fontSize: 42,
+              color: activeTextColor,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _getResultCard() {
+    Widget getCategory() {
+      return Expanded(
+        flex: 1,
+        child: Container(
+          child: Center(
+            child: Text(
+              "CATEGORY",
+              style: TextStyle(
+                color: Color(0xFF4BDD7B),
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
+    Widget getResult() {
+      return Expanded(
+        flex: 1,
+        child: Container(
+          child: Center(
+            child: Text(
+              "26.7",
+              style: TextStyle(
+                color: activeTextColor,
+                fontSize: 80,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
+    Widget getDescription() {
+      return Expanded(
+        flex: 1,
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: defaultMargin,
+          ),
+          child: Center(
+            child: Text(
+              "You have a higher than normal body weight. Try to exercise more.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: activeTextColor,
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
+    return Expanded(
+      flex: 7,
+      child: Container(
+        child: ReusableCard(
+          margin: EdgeInsets.only(
+            left: defaultMargin,
+            top: 3,
+            right: defaultMargin,
+            bottom: defaultMargin,
+          ),
+          color: activeCardColor,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              getCategory(),
+              getResult(),
+              getDescription(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _getButtonRecalculate() {
+    return FullWidthButton(
+      label: "RE-CALCULATE",
+      onPress: () {
+        AppRoutes.closePage(context);
+      },
     );
   }
 }
